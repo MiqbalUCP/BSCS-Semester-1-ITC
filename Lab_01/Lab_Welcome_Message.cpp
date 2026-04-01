65662c7aa949f6b1ae68e8d7cdcab750d5623eb0
Muhammad_iqbal_786 <miqbalucpian@gmail.com>
2026-04-01 23:17:02 +0500

    Delete Lab_01 directory

--- Lab_01/Lab_Welcome_Message.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout << "      Hello, Class!" << endl;
-	cout << "Welcome to the ITC-Lab-03" << endl;
-
-}
--- Lab_01/Variable_Initialization_Demo.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int x = 5;
-	cout << "The value of x is " << x << endl;;
-	return 0;
-}
