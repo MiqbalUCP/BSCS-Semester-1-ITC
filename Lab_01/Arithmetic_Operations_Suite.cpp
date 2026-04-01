b1cf9f1f2d5a5a2ac517db2f4a1eb1f02d216477
Muhammad_iqbal_786 <miqbalucpian@gmail.com>
2026-04-01 23:18:24 +0500

    Add Lab 01 Solution
    # Lab 01 – Introduction to C++ Programming
    
    This lab focuses on basic C++ concepts including:
    - Input and Output
    - Variables and Data Types
    - Arithmetic Operations
    - Simple Program Execution
    
    Course: Introduction To Computing Science 
    Semester: 1

---
+++ Lab_01/Arithmetic_Operations_Suite.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1, num2;
+	cout << "enter two integer numbers" << endl;
+	cout << "enter first integer" << endl;
+	cin >> num1;
+	cout << "enter second integer" << endl;
+	cin >> num2;
+	cout << " the sum of given integer " << num1 + num2 << endl;
+	cout << " the subtraction of given integer " << num1 - num2 << endl;
+	cout << " the multiplication of given integer " << num1 * num2 << endl;
+	cout << " the sum of division integer " << num1 / num2 << endl;
+	return 0;
+
+}
---
+++ Lab_01/Square_Calculator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout << " taking input from user" << endl;
+	cin >> num;
+	cout << "the square of given number is" << num * num << endl;
+	return 0;
+
+
+}
