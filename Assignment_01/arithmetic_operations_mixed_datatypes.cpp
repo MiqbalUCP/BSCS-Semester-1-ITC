99e44a0d6fbe3aa5cee4c1bbc887f1f85a3a32c0
Muhammad_iqbal_786 <miqbalucpian@gmail.com>
2026-04-06 23:13:24 +0500

    Add Assignment 01 Solution
    # Assignment 01 – Programming Fundamentals (C++)
    
    This assignment focuses on applying basic programming concepts using C++.
    
    Concepts Covered:
    - Variables and data types
    - User input and output
    - Conditional logic
    - Basic problem solving
    
    Course: Introduction To Computing Science
    Language: C++
    Semester: 1

---
+++ Assignment_01/arithmetic_operations_mixed_datatypes.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() 
+//{
+//    int n1, n2;
+//    float f1 = 4.7, f2 = 6.0;
+//
+//    cout << "Enter the first number: ";
+//    cin >> n1;
+//    cout << "Enter the second number: ";
+//    cin >> n2;
+//
+//    cout << "\nDisplay arithmetic operations with mixed data types:" << endl;
+//
+//    cout << "Addition:" << endl;
+//    cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
+//    cout << f1 << " + " << f2 << " = " << f1 + f2 << endl;
+//    cout << n1 << " + " << f2 << " = " << n1 + f2 << endl;
+//
+//    cout << "Subtraction:" << endl;
+//    cout << n1 << " - " << n2 << " = " << n1 - n2 << endl;
+//    cout << f1 << " - " << f2 << " = " << f1 - f2 << endl;
+//    cout << n1 << " - " << f2 << " = " << n1 - f2 << endl;
+//
+//    cout << "Multiplication:" << endl;
+//    cout << n1 << " * " << n2 << " = " << n1 * n2 << endl;
+//    cout << f1 << " * " << f2 << " = " << f1 * f2 << endl;
+//    cout << n1 << " * " << f2 << " = " << n1 * f2 << endl;
+//
+//    cout << "Division:" << endl;
+//    cout << n1 << " / " << n2 << " = " << (n1 / n2) << endl;
+//    cout << f1 << " / " << f2 << " = " << f1 / f2 << endl;
+//    cout << n1 << " / " << f2 << " = " << n1 / f2 << endl;
+//
+//    return 0;
+//}
